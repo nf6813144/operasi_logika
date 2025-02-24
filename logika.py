fe97914ed121dc6d9919ef504740f0a06f9d82d5
nf6813144 <nf6813144@gmail.com>
2025-02-24 09:43:05 +0800

    Create logika.py

---
+++ logika.py
@@ -0,0 +1,14 @@
+print("TABEL KEBENARAN KONJUNGSI, DISJUNGSI DAN -P OR Q")
+print("________________________________________________")
+
+print("P\t|Q\t|Q AND P\t|P OR |-P OR Q")
+print("________________________________________________")
+
+for P in [True, False]:
+    for Q in [True, False]:
+        Konjungsi = P and Q
+        Disjungsi = P or Q
+        Negasi = not -P or Q
+        print(f"{P}\t{Q}\t{Konjungsi}, {P}\t{Q}\t{Disjungsi},{-P}\t{Q}\t")
+
+print("__________________________________________________")
