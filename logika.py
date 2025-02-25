a0868d0300b176f2a11ecf6c25d98224eb2c7c0b
nf6813144 <nf6813144@gmail.com>
2025-02-26 07:12:00 +0800

    Update logika.py

--- logika.py
+++ logika.py
@@ -8,7 +8,7 @@ for P in [True, False]:
     for Q in [True, False]:
         Konjungsi = P and Q
         Disjungsi = P or Q
-        Negasi = not -P or Q
-        print(f"{P}\t{Q}\t{Konjungsi}\t{Disjungsi},\t{Negasi}")
+        Implikkasi = not -P or Q
+        print(f"{P}\t{Q}\t{Konjungsi}\t{Disjungsi},\t{Implikasi}")
 
 print("__________________________________________________")
